b3f950c852850782c23a6387de917ad9ac848ae2
Nilton S Ferreira <nsfgeo@hotmail.com>
2023-02-13 09:59:11 -0300

    Criado os personagens

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// let char = new Sorcerer("Nilton")
+// console.log(char.name);
+// console.log(char.life);
+// console.log(char.attack);
+
+let monster = new LittleMonster();
+console.log(monster.name);
+console.log(monster.life);
+console.log(monster.attack);
+
+let monster2 = new BigMonster();
+console.log(monster2.name);
+console.log(monster2.life);
+console.log(monster2.attack);
